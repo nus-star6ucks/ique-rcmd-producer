e1a792e08e1e345d82da042aad529786ada7bede
pcdotfan <i@know.cx>
2023-04-30 02:44:37 +0800

    cleanup

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const app = require("express")();
 const cors = require("cors");
+const { produce } = require("../producer");
+
 app.use(cors());
-
 app.set("port", process.env.PORT || 8081);
-
 app.get("/api/sendUserData", (req, res) => {
   const value = req.query;
-  produceExample(req.query.userId, value)
+  produce(req.query.userId, value)
     .then(() => {
       console.log("success");
       res.json({ success: true, msg: "send ok" });
@@ -17,72 +17,9 @@ app.get("/api/sendUserData", (req, res) => {
     });
 });
 
-app.listen(app.get("port"), () =>
-  console.log("Server running " + app.get("port"))
-);
+app.listen(app.get("port"), async () => {
+  console.log("Server running " + app.get("port"));
+});
 
-const Kafka = require("node-rdkafka");
-const { configFromPath } = require("./util");
-
-function createConfigMap(config) {
-  if (config.hasOwnProperty("security.protocol")) {
-    return {
-      "bootstrap.servers": config["bootstrap.servers"],
-      "sasl.username": config["sasl.username"],
-      "sasl.password": config["sasl.password"],
-      "security.protocol": config["security.protocol"],
-      "sasl.mechanisms": config["sasl.mechanisms"],
-      dr_msg_cb: true,
-    };
-  } else {
-    return {
-      "bootstrap.servers": config["bootstrap.servers"],
-      dr_msg_cb: true,
-    };
-  }
-}
-
-function createProducer(config, onDeliveryReport) {
-  const producer = new Kafka.Producer(createConfigMap(config));
-  return new Promise((resolve, reject) => {
-    producer
-      .on("ready", () => {
-        resolve(producer);
-      })
-      .on("delivery-report", onDeliveryReport)
-      .on("event.error", (err) => {
-        console.warn("event.error", err);
-        reject(err);
-      });
-    producer.connect();
-  });
-}
-
-async function produceExample(userId, storeValue) {
-  if (process.argv.length < 3) {
-    console.log(
-      "Please provide the configuration file path as the command line argument"
-    );
-    process.exit(1);
-  }
-  let configPath = process.argv.slice(2)[0];
-  const config = await configFromPath(configPath);
-
-  const topic = "user-info";
-  const producer = await createProducer(config, (err, report) => {
-    if (err) {
-      console.warn("Error producing", err);
-    } else {
-      console.log("Produced event to topic");
-    }
-  });
-  const key = Buffer.from(userId);
-  const value = Buffer.from(JSON.stringify(storeValue));
-
-  producer.produce(topic, -1, value, key);
-  producer.flush(10000, () => {
-    producer.disconnect();
-  });
-}
 
 module.exports = app;
---
+++ producer.js
@@ -0,0 +1,49 @@
+const Kafka = require("node-rdkafka");
+
+function configFromEnv() {
+  return {
+    "bootstrap.servers": process.env.bootstrap_servers,
+    sasl_username: process.env.sasl_username,
+    sasl_password: process.env.sasl_password,
+    security_protocol: process.env.security_protocol,
+    "sasl.sasl_mechanisms": process.env.sasl_mechanisms,
+    session_timeout_ms: process.env.session_timeout_ms,
+    dr_msg_cb: true,
+  };
+}
+
+function createProducer(onDeliveryReport) {
+  const producer = new Kafka.Producer(configFromEnv());
+  return new Promise((resolve, reject) => {
+    producer
+      .on("ready", () => {
+        resolve(producer);
+      })
+      .on("delivery-report", onDeliveryReport)
+      .on("event.error", (err) => {
+        console.warn("event.error", err);
+        reject(err);
+      });
+    producer.connect();
+  });
+}
+
+async function produce(userId, storeValue) {
+  const topic = "user-info";
+  const producer = await createProducer((err, report) => {
+    if (err) {
+      console.warn("Error producing", err);
+    } else {
+      console.log("Produced event to topic");
+    }
+  });
+  const key = Buffer.from(userId);
+  const value = Buffer.from(JSON.stringify(storeValue));
+
+  producer.produce(topic, -1, value, key);
+  producer.flush(10000, () => {
+    producer.disconnect();
+  });
+}
+
+module.exports = { produce };
